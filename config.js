f3bcaae37d20d0b1ca35028b2ad492fb92d74083
Tobias Lund-Melcher <ekst_tolm@eika.no>
2016-06-13 01:02:29 +0200

    Changed to correct mongo url

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion',
-    'facebook': {
-        clientID: '1',
-        clientSecret: 'fds',
-        callbackURL: 'https://localhost:3443/users/facebook/callback'
-    }
+    'mongoUrl': 'mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/saksarkiv'
 }
